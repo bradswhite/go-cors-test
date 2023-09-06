bb8a2d110e11b7aace45403ce2d1c9b7db0909ae
Brad White <bradscottwhite@gmail.com>
2023-09-06 13:22:28 -0400

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+const App = () => {
+  const [text, setText] = useState<string>('Loading...');
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch('http://localhost:8081/hello', {
+        method: 'GET',
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Headers': 'access-control-allow-origin, access-control-allow-headers'
+        },
+      });
+
+      setText(await res.text());
+    })();
+  }, []);
+
+  return (
+    <h1>{text}</h1>
+  );
+};
+
+export default App;
---
+++ server/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func main() {
+	app := fiber.New()
+
+    app.Use(cors.New(cors.Config{
+        AllowHeaders:     "Content-Type,access-control-allow-origin, access-control-allow-headers",
+        AllowOrigins:     "*",
+    }))
+
+	app.Get("/hello", func(c *fiber.Ctx) error {
+		return c.SendString("Hello world!")
+	})
+
+	log.Fatal(app.Listen(":8081"))
+}
